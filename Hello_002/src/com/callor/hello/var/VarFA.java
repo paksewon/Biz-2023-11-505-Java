0329c832a71014a588ed9a82bbd7c160307b9b7e
paksewon <qlrhdro2002@naver.com>
2023-11-22 16:42:55 +0900

    3일차 마무리

---
+++ Hello_002/src/com/callor/hello/var/VarFA.java
@@ -0,0 +1,23 @@
+package com.callor.hello.var;
+
+public class VarFA {
+	public static void main(String[] args) {
+		float scoreKor = 0;
+		float scoreEng = 0;
+		float scoreMath = 0;
+		float total = 0;	
+		float avg = 0;
+
+		
+		System.out.println("===========================");
+		System.out.println("국어 : " + scoreKor);
+		System.out.println("영어 : " + scoreEng);
+		System.out.println("수학 : " + scoreMath);
+		System.out.println("---------------------------");
+		System.out.println("총점 : " + total);
+		System.out.println("평균 : " + avg);
+		System.out.println("===========================");
+		
+	
+	}
+}
